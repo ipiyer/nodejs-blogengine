8a2eb7e1a61e8100ce4db977e5570a5daa7e1463
Prashanth <munichlinux@gmail.com>
2012-10-31 18:14:09 +0530

    Unfinished auth modules. Added models to the auth.

--- auth/login.js
+++ auth/login.js
@@ -1,10 +1,52 @@
+var models = require('./models.js');
+
+
+/*
+  method: GET
+  
+*/
+
+
 exports.loginPage = function(req, res) {
     res.render("auth/login.jade");
+    
 }
 
 
+/*
+  method : post
+  
+
+*/
+
 exports.loginUser = function(req, res) {
-    res.render('auth/login');
+    var param = req.body, email, password;
+
+    param.email? email = param.email.trim() : false ;
+    param.passwd? password = param.passwd.trim() : false ;
+
+    if(!email || !password) {
+        return res.json(404, { error: 'Parameters Required' });
+    }
+    
+    models.User().getByEmail(email, function(err, user){
+        if(!user || !user.isValidPasswd(password)) {
+            return res.json(403, {error: "Wrong username/password"});
+
+        }
+
+        
+
+        console.log("Logged in");
+
+        res.send(200, "Good user");
+
+        
+        
+    });
+
+    
+    
 }
 
 exports.logout = function(req, res) {
